978f0b281be0ed2b80f78bde0476a60ca9f3d07a
jooyb0831 <dia231@naver.com>
2025-03-27 15:15:47 +0900

    0327

--- Assets/Counter/Counter.cs
+++ Assets/Counter/Counter.cs
@@ -1,23 +1,56 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class Counter : MonoBehaviour
 {
     public Text CounterText;
-
+    public GameObject ballsParent;
+    public Transform[] balls;
     private int Count = 0;
 
     private void Start()
     {
-        Count = 0;
+        BallSet();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && balls != null)
+        {
+            RePos();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Count += 1;
         CounterText.text = "Count : " + Count;
+    }
+
+    void RePos()
+    {
+        Count = 0;
+        CounterText.text = "Count : " + Count;
+        foreach (var item in balls)
+        {
+            //item.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            item.position = new Vector3(Random.Range(-3.5f, 3.5f), Random.Range(12.0f, 13.0f), Random.Range(-3.5f, 3.5f));
+
+        }
+    }
+
+    void BallSet()
+    {
+
+        balls = ballsParent.GetComponentsInChildren<Transform>();
+        foreach (var item in balls)
+        {
+            item.transform.position = new Vector3(item.transform.position.x + Random.Range(-1f, 1f), 12.63f, item.transform.position.z + Random.Range(-1f, 1f));
+        }
     }
 }
